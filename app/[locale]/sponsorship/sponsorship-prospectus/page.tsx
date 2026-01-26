c14dde8ff1f2f067eedaace0c156440ff6a6b49f
Nattawut Pongkasem <nutnattawut@MacBook-Pro-khxng-Nattawut.local>
2026-01-26 09:09:35 +0700

    feat: Add sponsorship prospectus and confirmed sponsors pages, update navigation and content, and remove numerous unused image assets.

--- app/[locale]/sponsorship/sponsorship-prospectus/page.tsx
+++ app/[locale]/sponsorship/sponsorship-prospectus/page.tsx
@@ -3,9 +3,8 @@ import Layout from "@/components/layout/Layout"
 import Link from "next/link"
 import { useTranslations, useLocale } from 'next-intl';
 import SponsorshipProspectus from '@/components/sections/sponsorship/SponsorshipProspectus';
-import SponsorsList from '@/components/sections/sponsorship/SponsorsList';
 
-export default function Sponsorship() {
+export default function SponsorshipProspectusPage() {
     const tCommon = useTranslations('common');
     const t = useTranslations('sponsorship');
     const locale = useLocale();
@@ -16,19 +15,20 @@ export default function Sponsorship() {
                 <div className="inner-page-header" style={{ backgroundImage: 'url(/assets/img/bg/header-bg5.png)' }}>
                     <div className="container">
                         <div className="row">
-                            <div className="col-lg-8 m-auto">
+                            <div className="col-lg-9 m-auto">
                                 <div className="heading1 text-center">
-                                    <h1>{t('pageTitle')}</h1>
+                                    <h1>{t('sponsorshipProspectusMenu')}</h1>
                                     <div className="space20" />
-                                    <Link href={`/${locale}`}>{tCommon('home')} <i className="fa-solid fa-angle-right" /> <span>{t('pageTitle')}</span></Link>
+                                    <Link href={`/${locale}`}>{tCommon('home')} <i className="fa-solid fa-angle-right" /> <span>{t('pageTitle')}</span> <i className="fa-solid fa-angle-right" /> <span>{t('sponsorshipProspectusMenu')}</span></Link>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
+                <div className="space60" />
                 <SponsorshipProspectus />
-                <SponsorsList />
+                <div className="space60" />
             </div>
         </Layout>
     )
