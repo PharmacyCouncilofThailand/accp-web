bbb0677a55c471dfdf97710196149fa5401789ae
KoonArno <nattakarnklongkratok@gmail.com>
2026-07-09 17:25:49 +0700

    feat: add presentation schedule downloads component and supporting PDF asset

--- components/sections/program/PresentationScheduleDownloads.tsx
+++ components/sections/program/PresentationScheduleDownloads.tsx
@@ -16,8 +16,8 @@ const SCHEDULE_FILES = [
     },
     {
         id: 'poster',
-        href: '/assets/documents/poster-presentation-schedule-ACCP-2026.pdf',
-        downloadName: 'poster-presentation-schedule-ACCP-2026.pdf',
+        href: '/assets/documents/schedule/poster-presentation-schedule-ACCP 2026.pdf',
+        downloadName: 'poster-presentation-schedule-ACCP 2026.pdf',
         labelKey: 'downloadPosterSchedule',
         descriptionKey: 'downloadPosterScheduleDesc',
         icon: 'fa-image',
